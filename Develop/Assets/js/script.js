95d3d7777363cba2d21530300b6de606a92d7c4d
Kateryna Tekmenzhi <kateryna.koltunova@gmail.com>
2022-05-20 19:24:15 -0700

    updating README file

--- Develop/Assets/js/script.js
+++ Develop/Assets/js/script.js
@@ -1,14 +1,19 @@
+// selecting the savebtn and storing the value in local storage
 var saveBtn = document.querySelector(".saveBtn");
 
+// using moment to display the current day
 var currentDay = moment().format("MMM Do, YYYY");
 $("#currentDay").text(currentDay);
 
+// 
 function updateTime() {
+   // using moment to display the current time
    var currentTime = moment().hour();
     $("#currentTime").text(currentTime);
    
    console.log(currentTime, "currentTime");
 
+   // creating for loop to update the time with color coding
    for (i=7; i<19; i++) {
       var hourId = i.toString()
       if (i < currentTime) {
@@ -27,6 +32,7 @@ function updateTime() {
       };
 };
 
+// on click function to save the value in local storage
 $('.saveBtn').on('click', function () {
        var valSib = $(this).siblings('.description').val();    
        var timePar = $(this).parent().attr('id');    
@@ -40,6 +46,7 @@ $(".clear").on('click',function() {
 
 updateTime();
 
+// function with a for loop to display the saved value in local storage
 function renderTasks() {
    for (var i = 7; i < 19; i++) {
        $("#" + i).children("textarea").val(localStorage.getItem(i));
